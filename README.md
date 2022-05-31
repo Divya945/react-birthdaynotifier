b2802d196a31162a6c8da2be400b8a939001f5a4
Divya945 <divyadvs945@gmail.com>
2022-05-31 19:55:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-cu9g68
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-cu9g68)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import List from './Components/List'
+
+export default function App() {
+  return (
+    <div>
+      <List />
+    </div>
+  );
+}
